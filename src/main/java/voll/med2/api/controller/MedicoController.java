bb1a23ca737ceaca07f851c9f6e6519252265622
ALAN  MULERO DE TOLEDO <91330691+alanmulero@users.noreply.github.com>
2025-02-10 20:03:19 -0300

    Get pronto

--- src/main/java/voll/med2/api/controller/MedicoController.java
+++ src/main/java/voll/med2/api/controller/MedicoController.java
@@ -2,10 +2,15 @@ package voll.med2.api.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import voll.med2.api.medico.DTOListagemMedicos;
 import voll.med2.api.medico.DadosCadastroMedico;
 import voll.med2.api.medico.Medico;
 import voll.med2.api.medico.MedicoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -17,6 +22,12 @@ public class MedicoController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
             repository.save(new Medico(dados));
+    }
+
+    // Metodo Get
+    @GetMapping
+    public Page<DTOListagemMedicos> listar(Pageable paginacao) { // Paginação
+        return repository.findAll(paginacao).map(DTOListagemMedicos::new);
     }
 
 }
